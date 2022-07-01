7ea92f01414ed92f5fee65ddd7950e46fa250ff1
KholizaAlya <kholizalya@gmail.com>
2022-07-01 23:38:31 +0800

    Membuat KabupatenViewFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import helpers.Koneksi;
+import frame.KabupatenVIewFrame;
 public class Main {
     public static void main(String[] args) {
-        Koneksi.getConnection();
+//        Koneksi.getConnection();
+        KabupatenVIewFrame viewFrame = new KabupatenVIewFrame();
+        viewFrame.setVisible(true);
     }
+
 }
